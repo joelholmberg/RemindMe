09be1a7c7ce7326ae0bbc772dd5c36b81bc92b90
Joel Holmberg <joelholmberg88@gmail.com>
2013-04-13 16:39:23 +0200

    First commit with GitHub

---
+++ Notepadv3/src/com/android/demo/notepad3/AddPosition.java
@@ -0,0 +1,115 @@
+package com.android.demo.notepad3;
+
+import java.util.List;
+
+import com.example.remindme.MyItemizedOverlay;
+import com.example.remindme.R;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+import com.google.android.maps.Projection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddPosition  extends Activity {
+	private NotesDbAdapter mDbHelper;
+	private MapView mapView;
+	private LocationManager locManager;
+	private LocationListener locListener;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDbHelper = new NotesDbAdapter(this);
+        mDbHelper.open();
+        
+        setContentView(R.layout.add_position);
+        
+        setTitle(R.string.add_position);
+
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+        Button addPositionButton = (Button) findViewById(R.id.add_position);
+
+        mRowId = (savedInstanceState == null) ? null :
+            (Long) savedInstanceState.getSerializable(NotesDbAdapter.KEY_ROWID);
+        if (mRowId == null) {
+            Bundle extras = getIntent().getExtras();
+            mRowId = extras != null ? extras.getLong(NotesDbAdapter.KEY_ROWID)
+                                    : null;
+        }
+        
+    }
+	
+	/**
+	 * Initializes the MyLocationOverlay and adds it to the overlays of the map
+	 */
+	private void initLocationManager() {
+		locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		locListener = new LocationListener() {
+
+			public void onLocationChanged(Location newLocation) {
+				
+			}
+
+			public void onProviderDisabled(String arg0) {
+			}
+
+			public void onProviderEnabled(String arg0) {
+			}
+
+			public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+			}
+		};
+		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+				locListener);
+		locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+				locListener);
+		
+		
+	}
+	
+	/**
+	 * Initialize the map and adds the zoom controls to the LinearLayout.
+	 */
+	private void initMap() {
+		mapView = (MapView) findViewById(R.id.mymap);
+		mapView.setBuiltInZoomControls(true);
+		mapView.displayZoomControls(true);
+
+		//Get all overlays from mapView
+		List<Overlay> overlays = mapView.getOverlays();
+
+		// first remove old overlays
+		overlays.clear();
+
+		// initialize icon
+		Drawable icon = getResources().getDrawable(R.drawable.pin);
+		icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon
+				.getIntrinsicHeight());
+
+		// create new overlay
+		MyItemizedOverlay overlay = new MyItemizedOverlay(icon);
+		
+		//Add newly created overlay to overlays list
+		overlays.add(overlay);
+
+		// redraw map
+		mapView.postInvalidate();
+
+	}
+}
---
+++ Notepadv3/src/com/android/demo/notepad3/NoteEdit.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.demo.notepad3;
+
+import com.android.demo.notepad3.NotesDbAdapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NoteEdit extends Activity {
+	
+	private static final int ACTIVITY_ADD_POSITION=0;
+	
+	private NotesDbAdapter mDbHelper;
+    private EditText mTitleText;
+    private EditText mBodyText;
+    private Long mRowId;
+    private String mLatitude, mLongitude, mRadius;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDbHelper = new NotesDbAdapter(this);
+        mDbHelper.open();
+        
+        setContentView(R.layout.note_edit);
+        
+        setTitle(R.string.edit_note);
+
+        mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
+
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+        Button addPositionButton = (Button) findViewById(R.id.add_position);
+
+        mRowId = (savedInstanceState == null) ? null :
+            (Long) savedInstanceState.getSerializable(NotesDbAdapter.KEY_ROWID);
+        if (mRowId == null) {
+            Bundle extras = getIntent().getExtras();
+            mRowId = extras != null ? extras.getLong(NotesDbAdapter.KEY_ROWID)
+                                    : null;
+        }
+        
+        populateFields();
+        
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+            	setResult(RESULT_OK);
+                finish();
+            }
+        });
+    
+        
+        addPositionButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+            	addPosition();
+            }
+
+        });
+    }
+    
+    protected void addPosition() {
+    	Intent i = new Intent(this, AddPosition.class);
+        startActivityForResult(i, ACTIVITY_ADD_POSITION);
+	}
+
+	private void populateFields() {
+        if (mRowId != null) {
+            Cursor note = mDbHelper.fetchNote(mRowId);
+            startManagingCursor(note);
+            mTitleText.setText(note.getString(
+                        note.getColumnIndexOrThrow(NotesDbAdapter.KEY_TITLE)));
+            mBodyText.setText(note.getString(
+                    note.getColumnIndexOrThrow(NotesDbAdapter.KEY_BODY)));
+        }
+    }
+    
+    
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        saveState();
+        outState.putSerializable(NotesDbAdapter.KEY_ROWID, mRowId);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateFields();
+    }
+    
+    private void saveState() {
+        String title = mTitleText.getText().toString();
+        String body = mBodyText.getText().toString();
+
+        if (mRowId == null) {
+            long id = mDbHelper.createNote(title, body);
+            if (id > 0) {
+                mRowId = id;
+            }
+        } else {
+            mDbHelper.updateNote(mRowId, title, body);
+        }
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    	super.onActivityResult(requestCode, resultCode, intent);
+
+    	switch(requestCode) {
+    	case ACTIVITY_ADD_POSITION:
+    		//TODO: For future work
+    	}
+    }
+}
